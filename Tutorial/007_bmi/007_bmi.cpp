c9157b2f6221642c512cae46e47f0ea176ed3efd
BeeEye Dmu <bikang@konyang.ac.kr>
2023-09-26 17:59:14 +0900

    Add files via upload

---
+++ Tutorial/007_bmi/007_bmi.cpp
@@ -0,0 +1,22 @@
+﻿// 007_bmi.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	double height, weight;
+
+	printf("키를 입력(cm) : ");
+	scanf_s("%lf", &height);
+
+	printf("몸무게를 입력(kg) : ");
+	scanf_s("%lf", &weight);
+
+	printf("%6s : %6.1f\n", "키", height);
+	printf("%6s : %6.1f\n", "몸무게", weight);
+
+	height /= 100;	// cm를 m로 변환
+	double bmi = weight / (height * height);
+	printf("%6s : %6.1f\n", "bmi", bmi);
+}
---
+++ Tutorial/014_switch/014_switch.cpp
@@ -0,0 +1,43 @@
+﻿// 014_switch.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int score;
+
+    printf("점수를 입력하세요: ");
+    scanf_s("%d", &score);
+
+    switch (score / 5) {
+    case 20:
+    case 19:
+        printf("A+");
+        break;
+    case 18:
+        printf("A0");
+        break;
+    case 17:
+        printf("B+");
+        break;
+    case 16:
+        printf("B0");
+        break;
+    case 15:
+        printf("C+");
+        break;
+    case 14:
+        printf("C0");
+        break;
+    case 13:
+        printf("D+");
+        break;
+    case 12:
+        printf("D0");
+        break;
+    default:
+        printf("F");
+        break;
+    }
+}
