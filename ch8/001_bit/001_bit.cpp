673b4061c132665063be4cf15d1fe9672f4463fe
BeeEye Dmu <bikang@konyang.ac.kr>
2023-11-03 12:29:00 +0900

    Add files via upload

---
+++ ch8/001_bit/001_bit.cpp
@@ -0,0 +1,16 @@
+﻿// 001_bit.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+  int a = 4, b = 5;
+
+  printf("a & b = %d\n", a & b);
+  printf("a | b = %d\n", a | b);
+  printf("a ^ b = %d\n", a ^ b);
+  printf("~a = %d\n", ~a);
+  printf("a<<2 = %d\n", a << 2);
+  printf("a>>2 = %d\n", a >> 2);
+}
---
+++ ch8/002_define/002_define.cpp
@@ -0,0 +1,23 @@
+﻿// 002_define.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+#define PI 3.141592
+#define SQUARE(x) ((x)*(x))
+const double pi = 3.141592;
+
+enum color {red, blue, black};
+
+int main()
+{
+  color x;
+
+  x = black;
+  if (x == black)
+    printf("검은색 옷을 입었다\n");
+
+  printf("%f\n", PI); 
+  printf("%f\n", pi);
+  printf("%d\n", 100/SQUARE(5));  // 100/((5)*(5))
+  // (5+1)*(5+1) // 5+1*5+1
+}
