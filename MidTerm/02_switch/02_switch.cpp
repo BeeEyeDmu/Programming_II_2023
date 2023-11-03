602bc581e8d916477e89f1a0425616a63786fa9a
BeeEye Dmu <bikang@konyang.ac.kr>
2023-11-03 12:33:23 +0900

    Add files via upload

---
+++ MidTerm/02_switch/02_switch.cpp
@@ -0,0 +1,29 @@
+﻿// 02_switch.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{		
+	int score = 90;
+
+	switch (score/10)
+	{
+	case 10:
+	case 9:
+		printf("A\n");
+		break;
+	case 8:
+		printf("B\n");
+		break;
+	case 7:
+		printf("C\n");
+		break;
+	case 6:
+		printf("D\n");
+		break;
+	default:
+		printf("F\n");
+		break;
+	}
+}
---
+++ MidTerm/06_array/06_array.cpp
@@ -0,0 +1,31 @@
+﻿// 06_array.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+// n개의 값을 배열에 저장
+// 최소, 최대값을 출력
+#include <stdio.h>
+
+int main()
+{
+  int a[100];
+  int n;
+
+  printf("입력할 숫자의 갯수 : ");
+  scanf_s("%d", &n);
+
+  printf("n개의 숫자를 입력 : ");
+  for (int i = 0; i < n; i++)
+    scanf_s("%d", &a[i]);
+
+  int min = a[0];
+  int max = a[0];
+
+  for (int i = 1; i < n; i++) {
+    if (min > a[i])
+      min = a[i];
+    else if (max < a[i])
+      max = a[i];
+  }
+
+  printf("최대: %d, 최소:%d\n", max, min);
+}
