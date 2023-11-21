cd8b6072606c53dd64270fbd59073fbbb6c78278
BeeEye Dmu <bikang@konyang.ac.kr>
2023-11-21 17:23:55 +0900

    Add files via upload

---
+++ Tutorial/072_exponential/072_exponential.cpp
@@ -0,0 +1,42 @@
+﻿// 072_exponential.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+class Exponential {
+private:
+  int base;
+  int exp;
+public:
+  Exponential(int b = 1, int e = 1) {
+    this->base = b;
+    this->exp = e;
+  }
+  int getValue() { return (int)pow(base, exp);  }
+  int getBase() { return this->base; }
+  int getExp() { return this->exp; }
+  bool equals(Exponential b) {
+    if (pow(base, exp) == pow(b.base, b.exp))
+      return true;
+    else
+      return false;
+  }
+};
+
+int main()
+{
+  Exponential a(4, 2);  // 4의 2승
+  Exponential b(16);    // 16의 1승
+  Exponential c;        // 디폴트 생성자 사용(1의 1승)
+
+  cout << a.getValue() << " " << b.getValue() << " " << c.getValue() << endl;
+
+  cout << "a의 베이스 : " << a.getBase()
+    << ", 지수 : " << a.getExp() << endl;
+
+  if (a.equals(b))
+    cout << "같다" << endl;
+  else
+    cout << "다르다" << endl;
+}
---
+++ Tutorial/073_myDate/073_myDate.cpp
@@ -0,0 +1,35 @@
+﻿// 073_myDate.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+class MyDate {
+private:
+  int year;
+  int month;
+  int day;
+
+public:
+  MyDate(int year, int month, int day) {
+    this->year = year;
+    this->month = month;
+    this->day = day;
+  }
+
+  void show() {
+    cout << year << "년 " << month << "월 " << day << "일" << endl;
+  }
+};
+int main()
+{
+  MyDate christmas(2023, 12, 25);
+  MyDate parentsday(2024, 5, 8);
+
+  cout << "크리스마스 : ";
+  christmas.show();
+
+  cout << "어버이날 : ";
+  parentsday.show();
+
+}
