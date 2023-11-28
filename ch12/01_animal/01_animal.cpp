578ae2d407b92f5585711d0589f7dcfa87235691
BeeEye Dmu <bikang@konyang.ac.kr>
2023-11-28 17:46:16 +0900

    Add files via upload

---
+++ ch12/01_animal/01_animal.cpp
@@ -0,0 +1,52 @@
+﻿// 01_animal.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+  Animal() {
+    cout << "Animal() 생성자 호출" << endl;
+  }
+  void Breathe() {
+    cout << "숨을 쉰다" << endl;
+  }
+  int age;
+};
+
+class Dog : public Animal {
+public:
+  Dog() {
+    cout << "Dog() 생성자 호출" << endl;
+  }
+  void Walk() {
+    cout << "걷는다" << endl;
+  }
+};
+
+class Sparrow : public Animal {
+public:
+  Sparrow() {
+    cout << "Sparrow() 생성자 호출" << endl;
+  }
+  void Fly() {
+    cout << "난다" << endl;
+  }
+};
+
+int main()
+{
+  Dog d;
+  
+  d.age = 7;
+  d.Breathe();
+  d.Walk();
+  cout << d.age << endl;
+
+  Sparrow s;
+  s.age = 2;
+  s.Breathe();
+  s.Fly();
+  cout << s.age << endl;
+}
---
+++ ch12/02_Message/02_Message.cpp
@@ -0,0 +1,72 @@
+﻿// 02_Message.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Image {
+public:
+  operator string() {
+    return "사진";
+  }
+};
+
+class Message {
+private:
+  int sendTime;
+  string sendName;
+public:
+  Message(int sendTime, string sendName) {
+    this->sendTime = sendTime;
+    this->sendName = sendName;  
+  }
+  int GetSendTime() const { return sendTime; }
+  string GetSendName() const { return sendName; }
+};
+
+class TextMessage : public Message {
+private:
+  string text;
+public:
+  TextMessage(int sendTime, string sendName, string text) 
+    : Message(sendTime, sendName) { // 부모 클래스 생성자를 호출
+    this->text = text;
+  }
+  string GetText() const {
+    return text;
+  }
+};
+
+class ImageMessage : public Message {
+private:
+  Image* image;
+public:
+  ImageMessage(int sendTime, string sendName, Image* image)
+    : Message(sendTime, sendName) {
+    this->image = image;
+  }
+  Image* GetImage() const {
+    return image;
+  }
+};
+
+int main()
+{
+  Image* dogImage = new Image;
+  TextMessage* hello = new TextMessage(10, "강병익", "안녕");
+  ImageMessage* dog = new ImageMessage(20, "강병익", dogImage);
+
+  cout << "보낸 시간 : " << hello->GetSendTime() << endl;
+  cout << "보낸 사람 : " << hello->GetSendName() << endl;
+  cout << "내용 : " << hello->GetText() << endl;
+
+  cout << "보낸 시간 : " << dog->GetSendTime() << endl;
+  cout << "보낸 사람 : " << dog->GetSendName() << endl;
+  cout << "내용 : " << (string)*dog->GetImage() << endl;
+
+  delete dogImage;
+  delete hello;
+  delete dog;
+}
+
